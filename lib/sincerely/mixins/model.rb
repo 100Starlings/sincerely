c72cabe19f4d8a9ba7086c728210b08caca10838
Zoltan Iklodi <ziklodi@gmail.com>
2024-07-10 16:04:14 +0200

    RB-328 add state machine

--- lib/sincerely/mixins/model.rb
+++ lib/sincerely/mixins/model.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'aasm'
 require 'active_support/concern'
 
 module Sincerely
@@ -7,11 +8,57 @@ module Sincerely
     module Model
       extend ActiveSupport::Concern
 
-      included do
+      included do # rubocop:disable Metrics/BlockLength
+        include AASM
+
         serialize :delivery_options, coder: JSON
 
         validates :recipient, presence: true
         validates :notification_type, inclusion: { in: %w[email sms push] }
+
+        aasm column: :delivery_state do # rubocop:disable Metrics/BlockLength
+          state :draft, initial: true
+          state :accepted
+          state :rejected
+          state :delivered
+          state :bounced
+          state :complained
+          state :delayed
+          state :opened
+          state :clicked
+
+          event :set_accepted do
+            transitions to: :accepted, from: [:draft]
+          end
+
+          event :set_rejected do
+            transitions to: :rejected
+          end
+
+          event :set_delivered do
+            transitions to: :delivered
+          end
+
+          event :set_bounced do
+            transitions to: :bounced
+          end
+
+          event :set_complained do
+            transitions to: :complained
+          end
+
+          event :set_delayed do
+            transitions to: :delayed
+          end
+
+          event :set_opened do
+            transitions to: :opened
+          end
+
+          event :set_clicked do
+            transitions to: :clicked
+          end
+        end
       end
     end
   end
--- sincerely.gemspec
+++ sincerely.gemspec
@@ -26,7 +26,7 @@ Gem::Specification.new do |spec|
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  # spec.add_dependency 'example-gem', '~> 1.0'
+  spec.add_dependency 'aasm', '~> 5.3'
 
   spec.metadata['rubygems_mfa_required'] = 'true'
 end
