5fb6b2e479f2823d82d387aaa587d80a54cdc13a
Willian Gustavo Veiga <willian.veiga@100starlings.com>
2024-07-11 07:22:35 -0300

    [RB-327] Add anyway_config gem

---
+++ lib/config/sincerely_config.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Sincerely
+  class SincerelyConfig < ApplicationConfig
+    attr_config :model_name
+  end
+end
--- lib/sincerely.rb
+++ lib/sincerely.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'config/application_config'
+require_relative 'config/sincerely_config'
 require_relative 'generators/sincerely/install_generator'
 require_relative 'generators/sincerely/migration_generator'
 require_relative 'sincerely/mixins/model'
