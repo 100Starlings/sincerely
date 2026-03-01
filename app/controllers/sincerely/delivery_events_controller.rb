f71ebb1c046d4d9c2b6928e14655abbe4bb23b4c
Daniel Silva <djs@cin.ufpe.br>
2026-03-01 17:16:17 -0300

    add template and notification type filters to delivery events

--- app/controllers/sincerely/delivery_events_controller.rb
+++ app/controllers/sincerely/delivery_events_controller.rb
@@ -8,10 +8,19 @@ module Sincerely
       @events = @events.where(event_type: params[:event_type]) if params[:event_type].present?
       @events = @events.where('recipient LIKE ?', "%#{params[:recipient]}%") if params[:recipient].present?
 
+      if params[:template_id].present? || params[:notification_type].present?
+        notification_scope = notification_model.all
+        notification_scope = notification_scope.where(template_id: params[:template_id]) if params[:template_id].present?
+        notification_scope = notification_scope.where(notification_type: params[:notification_type]) if params[:notification_type].present?
+        message_ids = notification_scope.pluck(:message_id).compact
+        @events = @events.where(message_id: message_ids)
+      end
+
       @pagination = paginate(@events)
       @events = @pagination[:records]
 
       @event_types = Sincerely::DeliveryEvent.distinct.pluck(:event_type).compact
+      @templates = Sincerely::Templates::NotificationTemplate.all
     end
   end
 end
