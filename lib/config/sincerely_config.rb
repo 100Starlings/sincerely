af3bb7a1241a0b88ac03d4dd21b4f65e54e26f21
Daniel Silva <djs@cin.ufpe.br>
2026-03-15 10:11:18 -0300

    add return_url and return_label config options

--- lib/config/sincerely_config.rb
+++ lib/config/sincerely_config.rb
@@ -9,6 +9,10 @@ module Sincerely
     attr_config(notification_model_name: 'Notification')
     attr_config(:delivery_methods)
 
+    # Return navigation (optional)
+    # Set return_url to show a "back to app" link in the footer
+    attr_accessor :return_url, :return_label
+
     def delivery_methods
       as_json.dig('values', 'delivery_methods')
     end
