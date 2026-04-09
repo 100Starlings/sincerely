9bc424401a7ccdc6970299590642874cc7560164
Daniel Silva <djs@cin.ufpe.br>
2026-04-09 15:01:29 -0300

    fix: register sincerely assets via Sprockets 4 manifest instead of explicit list

--- lib/sincerely/engine.rb
+++ lib/sincerely/engine.rb
@@ -3,5 +3,9 @@
 module Sincerely
   class Engine < ::Rails::Engine
     isolate_namespace Sincerely
+
+    initializer 'sincerely.assets.precompile' do |app|
+      app.config.assets.precompile += %w[sincerely.js] if app.config.respond_to?(:assets)
+    end
   end
 end
